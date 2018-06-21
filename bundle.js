6ea2e5844d0199788df9ae28d2d8d8dc3ace30c8
Sunny Feijen <sunnyfeijen@wearenew.nl>
2018-06-21 11:40:28 +0200

    add leading per line

--- bundle.js
+++ bundle.js
@@ -12495,37 +12495,41 @@ $(document).ready(function() {
     $(this).find('p').each(function(i) {
 
       fontLoad(this, length, i);
+      setFont(this);
+      wrapLines(this);
+
+      setLeading(this, i);
 
       //set leading
-      var leading = $(this).attr('leading')+ 'pt';
-
-      if( vertical_align == 'top' ){
-        //remove leading from top line
-        if( leading != '' && typeof leading != 'undefined' && i == 0 ){
-
-        }
-
-        if( leading != '' && typeof leading != 'undefined' && i != 0 ){
-          // $(this).css("margin-top", leading);
-          setLeading( this );
-        }
-      }
-
-
-
-      if( vertical_align == 'bottom'){
-        //align baseline with bottom
-
-        if( leading != '' && typeof leading != 'undefined' && i != (length -1) ){
-          setLeading( this );
-        }
-
-        if( leading != '' && typeof leading != 'undefined' && i == (length -1) ){
-          setLeading( this );
-          //compensateLeading( this );
-        }
-
-      }
+      // var leading = $(this).attr('leading')+ 'pt';
+      //
+      // if( vertical_align == 'top' ){
+      //   //remove leading from top line
+      //   if( leading != '' && typeof leading != 'undefined' && i == 0 ){
+      //
+      //   }
+      //
+      //   if( leading != '' && typeof leading != 'undefined' && i != 0 ){
+      //     // $(this).css("margin-top", leading);
+      //     setLeading( this );
+      //   }
+      // }
+      //
+      //
+      //
+      // if( vertical_align == 'bottom'){
+      //   //align baseline with bottom
+      //
+      //   if( leading != '' && typeof leading != 'undefined' && i != (length -1) ){
+      //     setLeading( this );
+      //   }
+      //
+      //   if( leading != '' && typeof leading != 'undefined' && i == (length -1) ){
+      //     setLeading( this );
+      //     //compensateLeading( this );
+      //   }
+      //
+      // }
 
 
     });
@@ -12547,83 +12551,127 @@ $(document).ready(function() {
     var fontObj = opentype.load(font_path+'.ttf', function(err, font) {
       if (err) {
           console.log('Could not get metrics from ' + font_family + ' -> ' + err);
-          setFont(element, font, err, length, i);
       } else {
-        setFont(element, font, err, length, i);
+        compensateLeading(element, font, err, length, i)
         console.log(font_family+' metrics loaded');
       }
     });
 
   }
 
-  function setFont(element, font, err, length, i) {
+  function setFont(element) {
 
     // set font-family
     var fontFamily = $(element).attr('fontname');
     $(element).css("font-family", fontFamily);
 
-
     // set font size
     var fontSize = $(element).attr('font-size')+'pt';
     $(element).css("font-size", fontSize);
 
-    wrapLines(element);
+  }
+
+  function compensateLeading(element, font, err, length, i) {
+    //todo compensate leading for alignment
 
     if(!err){
+
+      var emUnits = font.unitsPerEm;
+      var ascender = font.tables.os2.sTypoAscender;
+      var descender = font.tables.os2.sTypoDescender;
+      var ascenderNormalized = ascender / emUnits
+      var descenderNormalized = descender / emUnits
+
+
+      if( window.navigator.platform.includes('win') !== -1 ){
+        // for Windows
+        var winAscent = font.tables.os2.usWinAscent;
+        var winDescent = font.tables.os2.usWinDescent;
+        ascenderNormalized = (winAscent / emUnits);
+        descenderNormalized = (winDescent / emUnits) * -1;
+
+      }
+      if( window.navigator.platform.includes('mac') !== -1 ){
+        // for Mac
+        var hheadAscent = font.tables.hhea.ascender;
+        var hheadDescent = font.tables.hhea.descender;
+        ascenderNormalized = (hheadAscent / emUnits);
+        descenderNormalized = (hheadDescent / emUnits);
+      }
+
+
+      var fontsize = $(element).attr('font-size');
 
       var vertical_align = $(element).closest('.block').attr('vertical-align');
       if( vertical_align == 'bottom' && i == (length -1) ){
         // aligned bottom and last paragraph
-        compensateLeading(element, font, vertical_align);
+
+        if( fontsize != '' && typeof fontsize != 'undefined' ){
+
+          var compensation = fontsize * descenderNormalized;
+          $(element).css("margin-bottom", compensation+'pt');
+
+        }
+
       }
 
     }
 
   }
 
-  function setLeading(element, font) {
 
-    var leading = $(element).attr('leading')+ 'pt';
-    $(element).css("margin-top", leading);
+  function wrapLines(element){
+
+    var spanInserted = $(element).html().split(" ").join(" </span><span>");
+    var wrapped = ("<span>").concat(spanInserted, "</span>");
+    $(element).html(wrapped);
+    var refPos = $(element).children('span').first().position().top;
+    var newPos;
+
+    $(element).find('span').each(function(index) {
+        newPos = $(this).position().top;
+        $(this).addClass('line');
+        if (index == 0){
+           return;
+        }
+        if (newPos == refPos){
+            $(this).prepend($(this).prev().text() + " ");
+            $(this).prev().remove();
+        }
+        refPos = newPos;
+    });
+
+  }
+
+
+
+  function setLeading(element, index) {
+
+    var fontSize = $(element).attr('font-size');
+    var leading = $(element).attr('leading');
+    leading = leading - fontSize;
+    if( leading < 0 ){
+      leading = 0;
+    }
+
+    $(element).find('.line').each(function(i) {
+
+      $(this).css("display", "block");
+      if( index == 0 && i == 0 ){
+        return
+      }
+
+      if( leading != '' && typeof leading != 'undefined' ){
+        leading = leading+ 'pt';
+        $(this).css("margin-top", leading);
+      }
+
+
+    })
 
   }
 
-  function compensateLeading(element, font, vertical_align) {
-    //todo compensate leading for alignment
 
-    var emUnits = font.unitsPerEm;
-    var ascender = font.tables.os2.sTypoAscender;
-    var descender = font.tables.os2.sTypoDescender;
-    var ascenderNormalized = ascender / emUnits
-    var descenderNormalized = descender / emUnits
-
-
-    if( window.navigator.platform.includes('win') !== -1 ){
-      // for Windows
-      var winAscent = font.tables.os2.usWinAscent;
-      var winDescent = font.tables.os2.usWinDescent;
-      ascenderNormalized = (winAscent / emUnits);
-      descenderNormalized = (winDescent / emUnits) * -1;
-
-    }
-    if( window.navigator.platform.includes('mac') !== -1 ){
-      // for Mac
-      var hheadAscent = font.tables.hhea.ascender;
-      var hheadDescent = font.tables.hhea.descender;
-      ascenderNormalized = (hheadAscent / emUnits);
-      descenderNormalized = (hheadDescent / emUnits);
-    }
-
-
-    var fontsize = $(element).attr('font-size');
-    if( fontsize != '' && typeof fontsize != 'undefined' ){
-
-      var compensation = fontsize * descenderNormalized;
-      $(element).css("margin-bottom", compensation+'pt');
-
-    }
-
-  }
 
 
 
@@ -12645,39 +12693,6 @@ $(document).ready(function() {
     // });
     //
     // quill.setText('Gandalf the Grey');
-
-  }
-
-
-  function wrapLines(element){
-
-    var spanInserted = $(element).html().split(" ").join(" </span><span class='line'>");
-    // console.log('spanInserted: '+spanInserted);
-    var wrapped = ("<span class='line'>").concat(spanInserted, "</span>");
-    // console.log('wrapped: '+wrapped);
-    $(element).html(wrapped);
-    // console.log($(element));
-    var refPos = $(element).first('span').position().top;
-    // console.log('refPos: '+refPos);
-    var newPos;
-
-    $(element).find('span').each(function(index) {
-        newPos = $(this).position();
-        // console.log(this);
-        // console.log('newPos: ');
-        // console.log(newPos);
-        if (index == 0){
-           return;
-        }
-        if (newPos == refPos){
-            $(this).prepend($(this).prev().text() + " ");
-            $(this).prev().remove();
-
-            //console.log(newPos);
-            //console.log(refPos);
-        }
-        refPos = newPos;
-    });
 
   }
 
